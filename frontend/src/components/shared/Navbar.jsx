32e28ab055f8af0b4fb1d6928f96eb17b2ed2cbf
Ruhulamin <ruhulamin4427@gmail.com>
2024-08-23 11:01:31 +0530

    implemented logout functinality

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -7,11 +7,31 @@ import React from "react";
 import { Button } from "../ui/button";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { LogOut, User2 } from "lucide-react";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { toast } from "sonner";
+import axios from "axios";
+import { setUser } from "@/redux/authSlice";
 
 function Navbar() {
   const {user} = useSelector(store=>store.auth);
+  const dispatch = useDispatch();
+ const navigate =  useNavigate()
+  const logoutHandler = async () => {
+    try {
+        const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+        if (res.data.success) {
+            dispatch(setUser(null));
+            navigate("/");
+            toast.success(res.data.message);
+        }
+    } catch (error) {
+        console.log(error);
+        toast.error(error.response.data.message);
+    }
+}
+
   return (
     <div className="bg-white">
       <div className="flex justify-between items-center max-w-7xl mx-auto p-5">
@@ -83,7 +103,7 @@ function Navbar() {
                     </div>
                     <div className="flex items-center ">
                       <LogOut />
-                      <Button variant="link">Logout</Button>
+                      <Button onClick={logoutHandler} variant="link">Logout</Button>
                     </div>
                   </div>
                 </div>
